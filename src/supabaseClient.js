985e4bae232ea75394814e7fcfd45e2d251c62ba
Shubham18-5-5-20 <shubham339712@gmail.com>
2025-06-15 16:54:56 +0530

    Feat: Use environment variables for Supabase credentials

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-
-// Replace the placeholder strings with your actual credentials.
-const supabaseUrl = 'https://iehorqrsbbpycqdjipap.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaG9ycXJzYmJweWNxZGppcGFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5ODAxNTgsImV4cCI6MjA2NTU1NjE1OH0.9hVB1u20N1oKZN2Q3dzxOwxMBVwVlKWB5mxsWoKjzhU'
+// Production ready Code
+// You can use this code to connect your React app to a Supabase database.
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // This creates the connection object that your app will use to talk to the database.
 export const supabase = createClient(supabaseUrl, supabaseKey)
